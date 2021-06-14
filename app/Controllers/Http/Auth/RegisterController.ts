73fe2d6f08903cac395a02115a50378488ee006d
Enrique Bonifácio <enriqueboni80@gmail.com>
2021-06-14 16:38:26 -0300

    configura adonis/mail

--- app/Controllers/Http/Auth/RegisterController.ts
+++ app/Controllers/Http/Auth/RegisterController.ts
@@ -1,6 +1,8 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 
+import Mail from '@ioc:Adonis/Addons/Mail'
+
 export default class RegisterController {
   public async index({}: HttpContextContract) {}
 
@@ -11,6 +13,18 @@ export default class RegisterController {
   public async store({ request }: HttpContextContract) {
     const data = request.only(['email', 'password'])
     const user = await User.create(data)
+
+    await Mail.send((message) => {
+      message
+        .from('info@example.com')
+        .to('virk@adonisjs.com')
+        .subject('Welcome Onboard!')
+        .htmlView('emails/welcome', {
+          user: { fullName: 'Some Name' },
+          url: 'https://your-app.com/verification-url',
+        })
+    })
+
     return user
   }
 
